bea5e1a55ced265273124d1ba439e692b42b2867
shiseki toshikazu <uw-t.a.k.n.s-10ve@ezweb.ne.jp>
2020-11-14 04:23:30 +0000

    [Add] お気に入り機能の追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
 
   root 'tweets#index'
-
   devise_for :users
-  resources :tweets
+
+  resources :tweets do
+  	resource :favorites, only: [:create, :destroy]
+  end
   resources :users
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
